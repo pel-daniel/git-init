a9ef573eb63c0b32252757c29ad2601d82218ae9
Daniel Garcia <pel_daniel@hotmail.com>
2016-05-27 22:43:32 -0500

    Extract 'commands' in array to reuse common code

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+var commands = [
+  {
+    animation: gitInit,
+    command: 'git init',
+    $element: '#git-init',
+    output: 'Initialized repo'
+  }
+]
+
 $(function() {
-  $('#git-init').click(gitInit)
+  commands.forEach(function(c) {
+    $(c.$element).click(animateCommand(c))
+  })
 })
 
-function gitInit(e) {
-  e.preventDefault()
-  e.stopPropagation()
+function animateCommand(command) {
+  return function(e) {
+    e.preventDefault()
+    e.stopPropagation()
 
+    command.animation()
+  }
+}
+
+function gitInit() {
   $('.area').show().animate({ height: '100%' }, 700, function() {
     $('.area-title').fadeIn(400)
   })
